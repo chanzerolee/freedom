efbe091e20547801128087363f6158c8e96a9994
chanzerolee <ybhuy8890@naver.com>
2023-03-31 01:06:03 +0900

    커밋 2 master main 어지럽다..

--- freedomgame/src/freedomgame/User.java
+++ freedomgame/src/freedomgame/User.java
@@ -3,4 +3,17 @@ package freedomgame;
 public class User {
 	int money;
 	int item;
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public int getItem() {
+		return item;
+	}
+	public void setItem(int item) {
+		this.item = item;
+	}
+	
 }
